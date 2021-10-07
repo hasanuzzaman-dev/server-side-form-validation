4ac34d0ba3dc90f357c3b67581509c854359018d
Hasan <hasanuzzaman.dev@gmail.com>
2021-10-07 18:33:58 +0600

    validation not working

--- src/main/java/com/hasan/serversidevalidation/controller/MyController.java
+++ src/main/java/com/hasan/serversidevalidation/controller/MyController.java
@@ -3,14 +3,35 @@ package com.hasan.serversidevalidation.controller;
 import com.hasan.serversidevalidation.models.LoginData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class MyController {
 
     @GetMapping("/form")
-    public String openForm(Model model){
-        model.addAttribute("loginData",new LoginData());
+    public String openForm(Model model) {
+        System.out.println("Open form");
+        model.addAttribute("loginData", new LoginData());
         return "form";
+    }
+
+    // handler for processing form
+    @PostMapping("/process")
+    public String processForm(@ModelAttribute("loginData") @Valid LoginData loginData, BindingResult result) {
+
+        if (result.hasErrors()) {
+            System.out.println(result);
+        }
+
+        System.out.println(loginData);
+
+        return "success";
+
     }
 }
--- src/main/java/com/hasan/serversidevalidation/models/LoginData.java
+++ src/main/java/com/hasan/serversidevalidation/models/LoginData.java
@@ -1,7 +1,20 @@
 package com.hasan.serversidevalidation.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
 public class LoginData {
+    @NotBlank(message = "Username can't be empty!")
+    @Size(min = 3,max = 12, message = "Username must be between 3-12!")
+    @NotEmpty(message = "Username can't be empty!")
     private String userName;
+
+
+    @NotBlank(message = "Email can't be empty!")
+    @Size(min = 3,max = 12, message = "Username must be between 3-12!")
+    @NotEmpty(message = "Username can't be empty!")
     private String email;
 
     public String getUserName() {
@@ -18,5 +31,13 @@ public class LoginData {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginData{" +
+                "userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
